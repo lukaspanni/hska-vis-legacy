ebf4b5580b5f7190f9ea13d6949529b859412c85
Lukas Panni <lukas.panni@outlook.de>
2022-06-09 14:23:19 +0200

    Update microservices, update build-images.bat

--- build-images.bat
+++ build-images.bat
@@ -2,6 +2,7 @@ cd nginx
 docker build -t nginx-proxy .
 cd ..
 docker build -t web-shop-db-image -f docker/DockerfileMySQL .
+docker build -t frontend -f docker/Dockerfile .
 cd microservices/category-service
 docker build -t category-service .
 cd ../product-service
